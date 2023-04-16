7735cd8d94cc641d75d25e937cc57d3dd0883ae5
jaugsbu2 <115282283+jaugsbu2@users.noreply.github.com>
2023-04-16 10:00:48 -0600

    adjust server package.json

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
+console.log('process.env', process.env.MONGODB_URI);
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
   }
 );
 
